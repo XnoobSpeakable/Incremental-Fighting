940d8a5818f15c66c4a2ee2e4e1cb008386822c1
XnoobSpeakable <xnoobspeakable@duck.com>
2024-10-27 16:36:59 +0200

    added more display logic

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Decimal from "break_eternity.js";
 
 const player = {
--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,18 @@ function updateTexts() {
     element("levelDisplay").textContent = `Level ${player.level}`
     element("levelNameDisplay").textContent = name[player.level]
     element("levelDifficultyDisplay").textContent = `Level difficulty: ${difficulty[player.level]}`
-    //we need to all all the other player and enemy stats to this
+
     element("healthDisplay").textContent = `Health: ${player.health.toFixed(2)}`;
+    element("attackSpeedDisplay").textContent = `Attack Speed: ${player.attackSpeed.toFixed(2)}`;
+    element("attackAccuracyDisplay").textContent = `Attack Accuracy: ${(100*player.attackAccuracy).toFixed(2)}%`;
+    element("baseStrengthDisplay").textContent = `Base Strength: ${player.baseStrength.toFixed(2)}`;
+    element("weaponMultiplierDisplay").textContent = `Weapon Multiplier: ${player.weaponMultiplier.toFixed(2)}`;
+    element("totalDamageDisplay").textContent = `Base Strength: ${totalDamage.toFixed(2)}`;
+
 	element("enemyHealthDisplay").textContent = `Enemy Health: ${currentEnemy.health.toFixed(2)}`;
 }
 
+let totalDamage = 0.1
 const TPS = 20;
 
 // game loop
@@ -30,10 +37,12 @@ setInterval(() => {
     if(isLevelFinished()) {
         element("levelUp").removeAttribute("disabled");
         element("h").textContent = "enabled"
+        currentEnemy.baseStrength = 0
     } else {
         element("levelUp").setAttribute("disabled", "disabled");
         element("h").textContent = "disabled"
     }
+    totalDamage = player.baseStrength * player.weaponMultiplier
 }, 1000 / TPS);
 
 // update loop
