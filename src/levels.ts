a42182ead0aa01ffcb019bccc603bd7d4960dd87
XnoobSpeakable <xnoobspeakable@duck.com>
2024-10-28 19:48:44 +0200

    add more lvls ig

--- src/levels.ts
+++ src/levels.ts
@@ -2,9 +2,23 @@ import player from "./data"
 import element from "./dom"
 import { generateEnemy, resetKills } from "./enemies"
 
-export const difficulty = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 10]
+export const difficulty = 
+    [1, 1.1, 1.2, 1.3, 1.4, //lvl 5
+     1.5, 1.6, 1.7, 1.8, 1.9, //lvl 10
+     2.5, 3, 3.5, 4, 4.5, 5, //lvl 15
+     5.5, 6, 6.5, 7, 10, //lvl 20
+     11, 12, 13, 14, 15, //lvl 25
+     16, 17, 18, 19, 30  //lvl 30
+    ]
 
-export const name = ['Tutorial', '', '']
+export const name = 
+    ['Tutorial', 'The beginning', '', '', '', //lvl 5
+     '', '', '', '', '', //lvl 10
+     'Slight jump in difficulty', '', '', '', '', //lvl 15
+     '', '', '', '', '', //lvl 20
+     '', '', '', '', '', //lvl 25
+     '', '', '', '', '' //lvl 30
+    ]
 
 export let levelEnemies = Math.ceil(player.level/10) + 1
 
